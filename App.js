a940648f8bb8e32c68f679adef46eda728deb2fd
kingsfieldakp <58221388+kingsfieldakp@users.noreply.github.com>
2020-11-24 21:55:09 +0000

    first reactnative project

--- App.js
+++ App.js
@@ -1,21 +1,155 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { TouchableOpacity, Text, View, StyleSheet, Image, TextInput } from 'react-native';
+import profile from './assets/profilepicture1.jpg';
+import profile1 from './assets/cam2.png';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+function App(){
+  return <View styles={styles.container}>
+    <Image source={profile} style={styles.image}/>
+    <Image source={profile1} style={styles.image1}/>
+
+    <View >
+      <Text style={styles.school}> school:</Text>
+      <TextInput style={styles.underline} placeholder="Codetrain" />
     </View>
-  );
+
+    <View>
+      <Text style={styles.school}>
+        name:
+      </Text>
+      <TextInput style={styles.underline} placeholder="Ave Maria" />
+    </View>
+
+    <View>
+      <Text style={styles.name}>
+        nickname:
+      </Text>
+      <TextInput style={styles.underline} placeholder="Ave" />
+    </View>
+
+    <View>
+      <Text style={styles.name}>
+        email:
+      </Text>
+      <TextInput style={styles.underline} placeholder="ave.m@codetrain.com" />
+    </View>
+
+    <View>
+      <Text style={styles.name}>
+        emergency contact:
+      </Text>
+      <TextInput style={styles.underline} placeholder="..." keyboardType={'numeric'} />
+    </View>
+
+    <View style={styles.button}>
+      <TouchableOpacity style={styles.buttonIn}>
+        <Text style={styles.buttontext}>UPDATE PROFILE</Text>
+      </TouchableOpacity>
+    </View>
+
+    
+
+    
+  </View>
+  
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const  styles = StyleSheet.create({
+  container:{
+    flex:1,
+    marginHorizontal:40
+    
+
   },
+  image:{
+    marginTop:60,
+    width:200,
+    height:200,
+    alignSelf:'center',
+    marginBottom:40,
+    borderRadius:200,  
+  },
+
+  image1:{
+    width:70,
+    height:70,
+    borderRadius:70,
+    position:'absolute',
+    marginTop:60,
+    left:220,
+    bottom:500
+    
+    
+
+  },
+ 
+  school:{
+    fontSize:17,
+    margin:3,
+    paddingHorizontal:35,
+    color:'gray'
+    
+    
+    
+  
+
+  },
+  sch_name:{
+    fontSize:17,
+    marginBottom:25,
+    
+    paddingHorizontal:35
+    
+    
+
+  },
+  button:{
+    marginTop:2,
+    width:'60%',
+    height:40,
+    justifyContent:'center',
+    alignSelf:'center',
+    alignContent:'center',
+    alignItems:'center',
+    marginTop:30
+    
+
+  },
+  buttonIn:{
+    
+    borderWidth:0,
+    height:42,
+    width:'100%',
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius:40,
+    backgroundColor:'#00BCD4'
+  },
+  buttontext:{
+    color:'white'
+  },
+  name:{
+    color:'#00BCD4',
+    paddingHorizontal:35,
+    margin:3,
+    fontSize:17
+    
+  },
+  underline:{
+    
+   width:300,
+   backgroundColor:'white',
+   marginHorizontal:25,
+   padding:16,
+   borderRadius:8,
+   borderBottomWidth:1,
+   borderBottomColor:'#f0f8ff',
+   marginBottom:7
+  
+
+  }
+
 });
+
+export default App;
